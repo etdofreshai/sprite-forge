228a79a54726b452737bf74825415c25c4a386ac
ETdoFresh <etdofresh@gmail.com>
2026-05-10 21:49:17 -0500

    fix: update Express 5 wildcard route syntax for SPA fallback

--- server.mjs
+++ server.mjs
@@ -83,7 +83,7 @@ async function startServer() {
   if (isProduction) {
     const distPath = path.join(__dirname, 'dist')
     app.use(express.static(distPath))
-    app.get('*', (_req, res) => {
+    app.get('/{*path}', (_req, res) => {
       res.sendFile(path.join(distPath, 'index.html'))
     })
   } else {
